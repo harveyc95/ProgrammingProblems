e3e2f02f2d9f8df0251af6809de7dabfc8703626
Harvey Chen <harveyc95@gmail.com>
2017-09-25 11:21:23 -0400

    added is_BST.cpp

---
+++ HackerRank/Verify_BST/is_BST.cpp
@@ -0,0 +1,18 @@
+The Node struct is defined as follows:
+   struct Node {
+      int data;
+      Node* left;
+      Node* right;
+   }
+*/
+#include <limits.h>
+
+bool checkBST(Node* root) {
+    return is_BST(root, INT_MIN, INT_MAX);
+}
+
+bool is_BST(Node* node, int min, int max) {
+    if (node == NULL)   return true;
+    if (node->data <= min || node->data >= max)   return false;
+    return is_BST(node->left, min, node->data) && is_BST(node->right, node->data, max);
+}
