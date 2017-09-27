0679d25a59bb337d5e060de16e5c670489f182a7
Harvey Chen <harveyc95@gmail.com>
2017-09-27 02:00:14 -0400

    added is_unique.cpp

---
+++ CTCI/Ch1/is_unique.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+bool is_unique (std::string s) {
+	if (s.length() > 128) return false;
+
+	bool char_list[128] = {0};
+
+	for (int i = 0; i < s.length(); i++) {
+		if (char_list[s[i]])	return false;
+		char_list[s[i]] = true;
+	}
+	return true;
+}
+
+int main () {
+	std::cout << is_unique("ABCDEFGA") << std::endl;
+}
