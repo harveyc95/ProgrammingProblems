5fe5ed0cf91c93b99e054029e5509f028d4a9344
Harvey Chen <harveyc95@gmail.com>
2017-09-27 16:51:42 -0400

    added hash_table_ranson_note.cpp

---
+++ HackerRank/hash_table_ranson_note.cpp
@@ -0,0 +1,58 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+bool ransom_note(vector<string> magazine, vector<string> ransom) {
+    std::map <string, int> map;
+    for (auto i : magazine) {
+        map[i] += 1;
+    }
+    for (auto j: ransom) {
+        if (map[j]) {
+            map[j] -= 1;
+        } else {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(){
+    int m;
+    int n;
+    cin >> m >> n;
+    vector<string> magazine(m);
+    for(int magazine_i = 0;magazine_i < m;magazine_i++){
+       cin >> magazine[magazine_i];
+    }
+    vector<string> ransom(n);
+    for(int ransom_i = 0;ransom_i < n;ransom_i++){
+       cin >> ransom[ransom_i];
+    }
+    if(ransom_note(magazine, ransom))
+        cout << "Yes\n";
+    else
+        cout << "No\n";
+    return 0;
+}
