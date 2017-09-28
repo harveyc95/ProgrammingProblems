2df703e61ec622e826aa5d770d5efc9f54efbf78
Harvey Chen <harveyc95@gmail.com>
2017-09-28 01:47:36 -0400

    added to_cap_space.cpp

---
+++ CTCI/Ch1/to_Cap_Case.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <iostream>
+
+std::string toCapCase(std::string s) {
+  int index = 0;
+  for (int i = 0; i < s.length(); i++) {
+    if (isspace(s[i]) or (i == s.length() - 1)) {
+    	s[index] = toupper(s[index]);
+    	index = i + 1;
+    }
+  }
+  return s;
+}
+
+int main () {
+
+	std::cout << toCapCase("Aaa bb cc dd ee ff gg hh i j k l m n o p q r s") << std::endl;
+
+	return 0;
+}
