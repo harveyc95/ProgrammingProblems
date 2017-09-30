c5e18af31b4577766219344a9d030333b1e0382c
Harvey Chen <harveyc95@gmail.com>
2017-09-29 23:44:49 -0400

    fixed 2d_matrix_rotate.cpp

--- CTCI/Ch1/2d_matrix_rotate.cpp
+++ CTCI/Ch1/2d_matrix_rotate.cpp
@@ -3,6 +3,36 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
+        int length = matrix.size();
+        for (int layer = 0; layer < length/2; layer++) {
+            int first = layer;
+            int last = length - layer - 1;
+            for (int i = first; i < last; i++) {
+                int offset = i - first;
 
+                // swap top and right
+                swap(matrix[first][i], matrix[i][last]);
+                // swap left and bot
+                swap(matrix[last-offset][first], matrix[last][last-offset]);
+                // swap top and bot
+                swap(matrix[first][i], matrix[last][last-offset]);
+                
+                // //save top
+                // int top = matrix[first][i];
+                
+                // // top = left
+                // matrix[first][i] = matrix[last-offset][first];
+                
+                // // left = bot
+                // matrix[last-offset][first] = matrix[last][last-offset];
+                
+                // // bot = right
+                // matrix[last][last-offset] = matrix[i][last];
+                
+                // // right = top
+                // matrix[i][last] = top;
+                
+            }
+        }
     }
 };
