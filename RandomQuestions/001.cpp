5bdbf1debecc7dd08de4b889c9f6137d66af53e4
Harvey Chen <harveyc95@gmail.com>
2017-10-01 17:04:56 -0400

    added 001.cpp

---
+++ RandomQuestions/001.cpp
@@ -0,0 +1,13 @@
+// 1. Round a number to the next largest multiple of 16
+
+#include <iostream>
+#include <climits>
+
+int round_to_largest_16(int x) {
+	return (x & 0xF) ? ((x >> 4 << 4) + 16) : x;
+}
+
+int main () {
+	std::cout<<round_to_largest_16(30)<<std::endl;
+	return 0;
+}
