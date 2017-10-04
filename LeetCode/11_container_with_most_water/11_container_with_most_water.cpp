324f32c92c3bc5f31095521c700df2f3e87f2ddb
Harvey Chen <harveyc95@gmail.com>
2017-10-04 01:03:08 -0400

    fixed 11_container_with_most_water.cpp

--- LeetCode/11_container_with_most_water/11_container_with_most_water.cpp
+++ LeetCode/11_container_with_most_water/11_container_with_most_water.cpp
@@ -4,13 +4,19 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int left, right, max_area;
+        int left, right, max_area = -1;
         left = 0;
-        right = height.size() - 1;
-        while (l > r) {
-            max_area = min(height[left], height[right]) * (right - left);
-            
+        right = height.size()-1;
+        while (left < right) {
+            max_area = max(max_area, (min(height[left], height[right]) * (right - left)));
+            if (height[right] < height[left]) {
+                right--;
+            } else {
+                left++;
+            }
         }
         return max_area;
     }
 };
+
+
