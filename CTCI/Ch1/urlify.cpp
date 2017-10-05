b560b23323b0008cbd0d37815e528c722aa57b11
Harvey Chen <harveyc95@gmail.com>
2017-10-05 17:36:26 -0400

    added urlify.cpp

---
+++ CTCI/Ch1/urlify.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+std::string urlify(std::string s) {
+
+	// remove trailing spaces
+	for (int i = s.length()-1; i >= 0; i--) {
+		if (isspace(s[i])) {
+			s.erase(i, 1);
+		} else {
+			break;
+		}
+	}
+
+	// replace spaces with %20
+	for (int i = 0; i < s.length(); i++) {
+		if (isspace(s[i])) {
+			s.erase(i, 1);
+			s.insert(i, "%20");
+		}
+	}
+
+	return s;
+}
+
+int main () {
+	urlify("A B C D E F G H I J K L M N                 ");
+	return 0;
+}
