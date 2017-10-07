49c5efc3af79bba1a995e666401e9f23d2f6db8f
Harvey Chen <harveyc95@gmail.com>
2017-10-06 21:58:40 -0400

    Update 2d_matrix_rotate.cpp

--- CTCI/Ch1/2d_matrix_rotate.cpp
+++ CTCI/Ch1/2d_matrix_rotate.cpp
@@ -19,22 +19,7 @@ public:
                 swap(matrix[last-offset][first], matrix[last][last-offset]);
                 // swap top and bot
                 swap(matrix[first][i], matrix[last][last-offset]);
-                
-                // //save top
-                // int top = matrix[first][i];
-                
-                // // top = left
-                // matrix[first][i] = matrix[last-offset][first];
-                
-                // // left = bot
-                // matrix[last-offset][first] = matrix[last][last-offset];
-                
-                // // bot = right
-                // matrix[last][last-offset] = matrix[i][last];
-                
-                // // right = top
-                // matrix[i][last] = top;
-                
+   
             }
         }
     }
