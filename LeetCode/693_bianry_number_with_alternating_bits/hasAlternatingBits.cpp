61b8fd825674f4bdd2035d6c643404ad47cc9d36
Harvey Chen <harveyc95@gmail.com>
2017-10-14 00:42:08 -0400

    added another solution to 693

--- LeetCode/693_bianry_number_with_alternating_bits/hasAlternatingBits.cpp
+++ LeetCode/693_bianry_number_with_alternating_bits/hasAlternatingBits.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool hasAlternatingBits(int n) {
+    bool hasAlternatingBits_1(int n) {
         if (n==0) return false;
         bool last = n&1;
         while (n>>=1 > 0) {
@@ -8,5 +8,10 @@ public:
             last = !last;
         }
         return true;
+    }
+
+    bool hasAlternatingBits_2(int n) {
+        int k = n + (n>>1) + 1;
+        return ((k & (k-1)) == 0);
     }
 };
