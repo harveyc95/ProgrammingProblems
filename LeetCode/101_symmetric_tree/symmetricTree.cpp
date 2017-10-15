8cbdaba3c508b7e75b3758b652e2549542a8f94c
Harvey Chen <harveyc95@gmail.com>
2017-10-15 00:13:44 -0400

    added 101 symmetricTree.cpp using BFS

---
+++ LeetCode/101_symmetric_tree/symmetricTree.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetricBFS(TreeNode* root) {
+        if (root == nullptr)    return true;
+        std::queue<TreeNode*> q1, q2;
+        q1.push(root->left);
+        q2.push(root->right);
+        while (!q1.empty() && !q2.empty()) {
+            TreeNode *n1 = q1.front(), *n2 = q2.front();
+            q1.pop();
+            q2.pop();
+            if (n1 == nullptr && n2 == nullptr)
+                continue;
+            if (n1 == nullptr || n2 == nullptr)
+                return false;
+            if (n1->val != n2->val)
+                return false;
+            q1.push(n1->left);
+            q1.push(n1->right);
+            q2.push(n2->right);
+            q2.push(n2->left);
+        }
+        return true;
+    }
+};
