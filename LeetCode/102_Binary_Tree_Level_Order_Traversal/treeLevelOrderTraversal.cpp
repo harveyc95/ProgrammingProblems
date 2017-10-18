ba505d2fcd84396ad4ffa3a4e19755e739a007fd
Harvey Chen <harveyc95@gmail.com>
2017-10-17 22:00:41 -0400

    added comments

--- LeetCode/102_Binary_Tree_Level_Order_Traversal/treeLevelOrderTraversal.cpp
+++ LeetCode/102_Binary_Tree_Level_Order_Traversal/treeLevelOrderTraversal.cpp
@@ -11,22 +11,22 @@ class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
         std::vector<vector<int>> ans;
-        std::vector<int> curr_level;
+        std::vector<int> currLevel;
         std::queue<TreeNode*> myq;
         if (root == nullptr)    return ans;
         myq.push(root);
-        myq.push(nullptr);
+        myq.push(nullptr); // marks the end of a level
         while(!myq.empty()) {
             TreeNode* node = myq.front();
             myq.pop();
             if (node == nullptr) { // end of level, copy level into ans
-                ans.push_back(curr_level);
-                curr_level.clear();
-                if (myq.size() > 0)
+                ans.push_back(currLevel);
+                currLevel.clear();
+                if (myq.size() > 0) // add another null end of queue, otherwise infinite loop
                     myq.push(nullptr);
             }
             else {
-                curr_level.push_back(node->val);
+                currLevel.push_back(node->val);
                 if (node->left != nullptr)  myq.push(node->left);
                 if (node->right != nullptr) myq.push(node->right);
             }
