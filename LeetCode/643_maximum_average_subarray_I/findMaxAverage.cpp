cc672ba257e127116bbabfc46e85e4e6420354dd
Harvey Chen <harveyc95@gmail.com>
2017-10-22 00:32:24 -0400

    leetcode 643 with sliding window

--- LeetCode/643_maximum_average_subarray_I/findMaxAverage.cpp
+++ LeetCode/643_maximum_average_subarray_I/findMaxAverage.cpp
@@ -11,4 +11,16 @@ public:
         }
         return maxAvg;
     }
+    double findMaxAverage(vector<int>& nums, int k) {
+        double window;
+        for (int i = 0; i < k; i++) window += nums[i];
+
+        double tmp = window;
+        for (int i = 1; i <= nums.size() - k; i++) {
+            std::cout<<nums[i-1]<<":"<<nums[i+k-1]<<":"<<tmp<<endl;
+            tmp = nums[i+k-1] - nums[i-1] + window;
+            window = max(tmp, window);
+        }   
+        return (double)window/k;
+    }
 };
