dfe1b920980aa6981e752e880a174fcdd7ad6f51
Harvey Chen <harveyc95@gmail.com>
2017-11-01 00:22:58 -0400

    updated removeDupe.cpp

--- LeetCode/26_remove_duplicate_from_sorted_array/removeDupe.cpp
+++ LeetCode/26_remove_duplicate_from_sorted_array/removeDupe.cpp
@@ -5,19 +5,12 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         if (nums.empty()) return 0;
-        if (nums.size() == 1) return 1;
-        int size = nums.size();
-        int i = 1;
-        int last = nums[0];
-        while (i < size) {
-            if (last == nums[i]) {
-                nums.erase(nums.begin() + i);
-                size--;
-            } else {
-                last = nums[i];
-                i++;
+        int writing = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[writing-1] != nums[i]) {
+                nums[writing++] = nums[i];
             }
         }
-        return i;
+        return writing;
     }
 };
