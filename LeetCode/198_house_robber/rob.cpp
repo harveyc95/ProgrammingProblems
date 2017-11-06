083d64a6a143931fe45092dbe9658cfe90abf541
Harvey Chen <harveyc95@gmail.com>
2017-11-06 12:29:49 -0500

    198 renamed variables

--- LeetCode/198_house_robber/rob.cpp
+++ LeetCode/198_house_robber/rob.cpp
@@ -8,10 +8,11 @@ public:
         dp[1] = nums[1];
         if (nums.size() == 2)   return max(dp[0], dp[1]);
         int maxMoney = max(dp[0], dp[1]);
-        int maxBefore = 0;
+        int maxBeforeNeighbour = 0;
         for (int i = 2; i < nums.size(); i++) {
-            maxBefore = max(maxBefore, dp[i-2]);
-            maxMoney = max(nums[i]+maxBefore,dp[i-1]);
+            int neighbour = dp[i-1];
+            maxBeforeNeighbour = max(maxBeforeNeighbour, dp[i-2]);
+            maxMoney = max(nums[i]+maxBeforeNeighbour,neighbour);
             dp[i] = maxMoney;
         }
         return maxMoney;
